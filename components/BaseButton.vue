d1a3fc7ccb974070104ec2848fd8d7eb8b6715ad
Martijn Halekor <martijn@cardan.com>
2025-04-16 12:18:30 +0200

    first commit

---
+++ components/BaseButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ label: string }>();
+
+const buttonRef = useTemplateRef('btn');
+</script>
+
+<template>
+  <button ref="btn">{{ label }}</button>
+</template>
